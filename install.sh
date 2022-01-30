c05f9a68a8180504a54bd0a8b414a247ac70fd2a
erick <ericktomas691@gmail.com>
2022-01-30 16:59:22 -0600

    add Readme.md

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 echo "Instalando bspwm"
 sleep 5
 # Instalar programas de pacman 
-sudo pacman -S htop tilix rofi bspwm sxhkd polybar feh xorg-xsetroot numlockx neovim nautilus cmus lightdm scrot lxappearance-gtk3 
-
-# virtualbox telegram-desktop 
+sudo pacman -S htop tilix rofi bspwm sxhkd polybar feh xorg-xsetroot numlockx neovim cmus lightdm lightdm-gtk-greeter scrot lxappearance-gtk3
+# sudo pacman -S virtualbox telegram-desktop nautilus
 
 # Instalar programas de aur
-yay -S google-chrome visual-studio-code-bin 
+# yay -S google-chrome
+# yay -S visual-studio-code-bin 
 sleep 5
 echo "Instalacion completada"
 sleep 5
