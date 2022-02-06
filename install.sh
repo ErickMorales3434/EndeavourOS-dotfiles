924c6b74956a62bdad7ec37f1956ff3d27201f9c
ErickMorales3434 <70081900+ErickMorales3434@users.noreply.github.com>
2022-02-06 17:16:52 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 echo "Instalando bspwm"
 sleep 5
 # Instalar programas de pacman 
-sudo pacman -S htop tilix rofi bspwm sxhkd polybar feh xorg-xsetroot numlockx neovim cmus lightdm lightdm-gtk-greeter scrot lxappearance-gtk3
-# sudo pacman -S virtualbox telegram-desktop nautilus
+sudo pacman -S htop tilix rofi bspwm sxhkd polybar feh xorg-xsetroot numlockx neovim cmus lightdm lightdm-gtk-greeter scrot lxappearance-gtk3 virtualbox telegram-desktop nautilus
 
 # Instalar programas de aur
-# yay -S google-chrome
-# yay -S visual-studio-code-bin 
+yay -S google-chrome
+yay -S visual-studio-code-bin 
 sleep 5
 echo "Instalacion completada"
 sleep 5
@@ -14,7 +13,6 @@ sleep 5
 sudo systemctl enable lightdm
 
 cp -r config/* ~/.config
-
 cp -r fonts ~/.fonts
 cp -r themes ~/.themes
 cp xinitrc ~/.xinitrc
