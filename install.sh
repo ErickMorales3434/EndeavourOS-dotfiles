9d0de0eb4fbceea5fb6efb8c5b0d55a5dd01f2e1
Erick Morales <ericktomas691@gmail.com>
2022-03-16 22:58:59 -0600

    new dotfiles

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
+# $ eval "$(ssh-agent -s)"
+
 echo "Instalando bspwm"
 sleep 5
 # Instalar programas de pacman 
-sudo pacman -S htop tilix rofi bspwm sxhkd polybar picom feh xorg-xsetroot numlockx neovim cmus lightdm lightdm-gtk-greeter scrot lxappearance-gtk3 virtualbox telegram-desktop nautilus python-pynvim obs-studio playerctl vlc gparted nitrogen volumeicon zsh
+sudo pacman -S kitty rofi bspwm sxhkd polybar picom feh xorg-xsetroot numlockx neovim cmus lightdm lightdm-gtk-greeter scrot lxappearance-gtk3 virtualbox telegram-desktop python-pynvim obs-studio playerctl vlc gparted volumeicon zsh ranger w3m
 
 # Instalar programas de aur
 yay -S google-chrome
@@ -15,6 +17,8 @@ sudo systemctl enable lightdm
 cp -r config/* ~/.config
 cp -r fonts ~/.fonts
 cp -r themes ~/.themes
+cp -r ssh ~/.ssh
+
 cp xinitrc ~/.xinitrc
 cp -r Wall ~/Wall
 
@@ -23,9 +27,10 @@ cp -r Wall ~/Wall
 # neovim plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+EDITOR=code
+
 #config zsh
 chsh -s $(which zsh)
-
 #install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
