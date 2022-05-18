23fb3d85d77c164062f62999d9181738105a4255
tomas <ericktomas691@gmail.com>
2022-05-18 00:30:10 -0600

    delete autorun.sh and git_automatization

--- config/bspwm/git_bspwm.py
+++
@@ -1,6 +0,0 @@
-# import sys
-import subprocess
-# dato = sys.argv[1]
-print("imprime este dato por favor")
-subprocess.run(["~/Endeavouros-fotfiles"],shell=True)
-subprocess.run(["git status "],shell=True)
