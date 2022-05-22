927cdedced0356926e4a0d6e7e1d27e7d8e5d346
tomas <ericktomas691@gmail.com>
2022-05-21 19:34:54 -0600

    add ulauncher

--- config/bspwm/autorun.py
+++ config/bspwm/autorun.py
@@ -6,7 +6,7 @@ import subprocess
 def main():
     directorios = ["Documentos","Descargas","Imágenes","Música","Vídeos"]
     for directorio in directorios:
-        busqueda = subprocess.run(["cp -r -u -v ~/{} /run/media/erick/1DT/LINUX".format(directorio)],shell=True,capture_output=True)
+        busqueda = subprocess.run(["cp -r -u -v ~/{} /run/media/erick/1TD/LINUX".format(directorio)],shell=True,capture_output=True)
 
     if busqueda.returncode == 0:
             subprocess.run(['''dunstify "Backup datos personales" "La copia se ha finalizado con exito 🙂" -u normal'''],shell=True)
---
+++ config/bspwm/git_bspwm.py
@@ -0,0 +1,6 @@
+# import sys
+import subprocess
+# dato = sys.argv[1]
+print("imprime este dato por favor")
+subprocess.run(["~/Endeavouros-fotfiles"],shell=True)
+subprocess.run(["git status "],shell=True)
